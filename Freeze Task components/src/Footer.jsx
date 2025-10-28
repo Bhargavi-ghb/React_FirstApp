c56d45ee8d13aa344506c42a29f7bb1fb6088617
Bhargavi_ghb <bhargavimaddula10@gmail.com>
2025-10-28 11:32:23 +0530

    updated

--- Freeze Task components/src/Footer.jsx
+++ Freeze Task components/src/Footer.jsx
@@ -1,20 +1,19 @@
 import React from "react";
 class Footer extends React.Component {
-   
+
     render() {
         return (
 
             <>
-                <div style={{backgroundColor:"#cb3362",padding:"20px"}}>
-                      <div style={{display:"flex",gap:"70px",justifyContent:"center"}}>
-                    <img width="50" height="50" src="https://img.icons8.com/ios/50/facebook-new.png" alt="facebook-new" />
-                    <img width="50" height="50" src="https://img.icons8.com/ios/50/whatsapp--v1.png" alt="whatsapp--v1" />					
-                    <img width="50" height="50" src="https://img.icons8.com/ios-filled/50/twitterx--v2.png" alt="twitterx--v2" />           
-                <img width="48" height="48" src="https://img.icons8.com/color/48/linkedin.png" alt="linkedin" />
-                </div> 
-                <p style={{textAlign:"center"}}>© 2023 Freeeze. All Rights Reserved.</p>
+                <div style={{ backgroundColor: "#cb3362", padding: "20px" }}>
+                    <div style={{ display: "flex", gap: "70px", justifyContent: "center" }}>
+                        <img width="50" height="50" src="https://img.icons8.com/ios/50/facebook-new.png" alt="facebook-new" />
+                        <img width="50" height="50" src="https://img.icons8.com/ios/50/whatsapp--v1.png" alt="whatsapp--v1" />
+                        <img width="50" height="50" src="https://img.icons8.com/ios-filled/50/twitterx--v2.png" alt="twitterx--v2" />
+                        <img width="50" height="50" src="https://img.icons8.com/ios/50/linkedin.png" alt="linkedin" />                </div>
+                    <p style={{ textAlign: "center" }}>© 2023 Freeeze. All Rights Reserved.</p>
                 </div>
-              
+
             </>
         );
     }
